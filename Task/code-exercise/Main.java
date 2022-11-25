aa4b73f839feaf5c00eada4b86c59f6a4d6bb991
kevmurr <31069141+kevmurr@users.noreply.github.com>
2022-11-25 17:04:20 +0100

    Add files via upload

---
+++ Task/code-exercise/Main.java
@@ -0,0 +1,17 @@
+package com.hlag.cities;
+
+
+public class Main {
+
+    static String defaultString = "(Hamburg, Germany, [1,2,3,4]); (Jeddah, Saudi-Arabia, [5,3,7,4]); (Singapore, Singapore, [2,3]); (Los Angeles, USA, [4,3,8,7])";
+
+    public static void main(String[] args) {
+        // Read all steps before starting to implement:
+        // 1. Please create a command-line application in an IDE of your choice
+        // 2. Create a class to store a City, with an id, a name, a country and an ordered collection of Ratings (integers).
+        //       The id should be unique for each city. Make sure that the values cannot be changed after creation, only additional ratings may be added.
+        // 3. Create Instances of your Class from the String above [defaultSting] (the String format is fixed as is) and store them in a manner that allows access based on the id
+        // 4. present 2 Ways to output the information of the Object to StdOut
+        // 5. Finally, Sort the Cities based on the average rating and Print the Ranking in order starting with the highest average
+    }
+}
